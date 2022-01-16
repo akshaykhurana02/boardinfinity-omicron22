9e87476f0500e39dceaff451c2bcc9924512239c
Akshay Khurana <akshaykhurana@Akshays-MacBook-Air.local>
2022-01-16 21:28:22 +0530

    16th January Material

---
+++ JavaScript/second-js.js
@@ -0,0 +1,54 @@
+var firstName = "Akshay";
+var lastName = "Khurana";
+var fullName;
+
+//console.log(firstName); // Akshay
+//console.log(lastName); // Khurana
+//console.log(fullName); // undefined
+
+
+fullName = firstName + " " + lastName; // New Value is given to Full Name
+
+//console.log(fullName); // Akshay Khurana
+
+
+for(var i = 1; i <=5; i++) {
+    console.log(firstName); // Akshay
+}
+
+
+/*
+
+    Logic
+
+    i = 1, 1 <= 5 -> True -> Print Akshay -> Value will be incremented to 2
+    i = 2; 2 <= 5 -> True -> Print Akshay -> Value will be incremented to 3
+    i = 3; 3 <= 5 -> True -> Print Akshay -> Value will be incremented to 4
+    i = 4; 4 <= 5 -> True -> Print Akshay -> Value will be incremented to 5
+    i = 5; 5 <= 5 -> True -> Print Akshay -> Value will be incremented to 6
+    i = 6; 6 <= 5 -> False -> Exit from Loop without printing anything
+
+*/
+
+
+/*
+    Assignments 16th January 2022
+
+    Assignment 1
+
+    5 * 1 = 5
+    5 * 2 = 10
+    5 * 3 = 15
+    .
+    .
+    .
+    5 * 10 = 50
+
+
+    Assignment 2
+
+
+    Read about Datatypes, operators, let & const in JavaScript
+
+
+*/
