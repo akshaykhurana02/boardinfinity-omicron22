3a67ebefec5b12fd300e6686541be41ae50de5f2
Akshay Khurana <akshaykhurana@Akshays-MacBook-Air.local>
2022-02-16 22:32:56 +0530

    16 Feb Material

---
+++ JavaScript/16 Feb/assignment.js
@@ -0,0 +1,10 @@
+// 1. Read about Callback Function
+// 2. Read about Higher Order Function
+// 3. 
+
+/*
+    // Keys & Values are same
+    // Keys & Values are different
+*/
+
+// 4. Check whether there are equal no. of keys and values in a map
