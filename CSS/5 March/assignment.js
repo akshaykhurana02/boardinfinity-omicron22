207d25074bc5f80a70bc18f867a4ecbe3279b822
Akshay Khurana <akshaykhurana@Akshays-MacBook-Air.local>
2022-03-05 20:32:24 +0530

    5th March Study Material

---
+++ CSS/5 March/assignment.js
@@ -0,0 +1,13 @@
+/*
+        Assignment 1 ->
+
+        Create a Table with following records ->
+
+
+        First Name, Last Name, Roll No., Gender
+
+        Alternate rows colors should be different (External CSS)
+
+        At the bottom, there should be a link to some other page...
+
+*/
