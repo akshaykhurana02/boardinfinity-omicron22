426974de124970bddef6138bd4a3ebbe2834be74
Akshay Khurana <akshaykhurana@Akshays-MacBook-Air.local>
2022-03-12 20:42:46 +0530

    12th March Material

---
+++ CSS/12 Mar/assignment.js
@@ -0,0 +1,17 @@
+/*
+
+Create a Food Outlet Website covering following topics -
+
+CSS Margins
+CSS Padding
+CSS Border
+Opacity
+
+Table
+Images
+Forms
+
+Add Animations if possible
+
+
+*/
---
+++ CSS/12 Mar/topics.js
@@ -0,0 +1,14 @@
+// Topics
+
+/*
+
+    CSS Height/Width    
+    CSS Fonts
+    CSS List
+    CSS Tables
+    Combinators
+
+    Position in CSS
+    Psuedo Classes
+
+*/
