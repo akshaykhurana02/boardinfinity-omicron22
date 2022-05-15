9d4c3af275e5c93b61def349cb314c4112f08d92
Akshay Khurana <akshaykhurana@Akshays-MacBook-Air.local>
2022-05-15 16:27:07 +0530

    15th May Material

--- React/14 May/pics/src/components/App.js
+++ React/14 May/pics/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
@@ -18,6 +19,7 @@ class App extends React.Component {
         return (
             <div className='ui container' style={{ marginTop: '15px' }}>
                 <SearchBar onTextSubmit={this.onSearchSubmit} /> {/* Callback */}
+                <ImageList />
             </div>
         );
     }
